800cb004132023ac81f862b1864abc45d2f107bc
Jorge Alexandre <jorgedbl309@gmail.com>
2025-08-17 11:54:13 -0300

    add client export to excel. Added Get route /export/clients to download the spreadsheet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const Clients_routes = require('./routes/Clients_routes');
+const exports_routes = require('./routes/exports_routes');
 
 app.use(cors());
 
@@ -9,5 +10,6 @@ app.use(express.json());
 
 app.use('/Customer', Clients_routes);
 
+app.use('/', exports_routes);
 
 module.exports = app;
