bdc474ed667884f10fc825aa6e72997014b68d11
JorgeAlexandresantosfilho <jorgedbl309@gmail.com>
2025-08-28 17:39:16 -0300

    corrijindo os erros no crud de planos, controllers e models. agora ja é possivel consumir rotas de planos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const Clients_routes = require('./routes/Clients_routes');
 const exports_routes = require('./routes/exports_routes');
+const plans_controller = require('./routes/plans_routes');
 
 app.use(cors());
 
@@ -11,5 +12,7 @@ app.use(express.json());
 app.use('/Customer', Clients_routes);
 
 app.use('/', exports_routes);
+
+app.use('/Plans', plans_controller);
 
 module.exports = app;
