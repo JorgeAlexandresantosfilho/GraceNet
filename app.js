c1361ee6d0a6d4b9bf1dde9bb1b830a33d92e2e7
JorgeAlexandresantosfilho <jorgedbl309@gmail.com>
2025-09-28 18:47:43 -0300

    Merge resolvido e arquivos prontos para push

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -20,23 +19,5 @@ app.use('/Plans', plans_routes);
 
 app.use('/Support', os_routes);
 
-=======
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const Clients_routes = require('./routes/Clients_routes');
-const exports_routes = require('./routes/exports_routes');
-const plans_controller = require('./routes/plans_routes');
 
-app.use(cors());
-
-app.use(express.json());
-
-app.use('/Customer', Clients_routes);
-
-app.use('/', exports_routes);
-
-app.use('/Plans', plans_controller);
-
->>>>>>> eb502bf00be535d061ae0d80bd7706d57f4f4bb4
 module.exports = app;
--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -12,20 +11,5 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT
 });
 
-module.exports = pool.promise();
-=======
-const dotenv = require('dotenv');
-dotenv.config();
-
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
-});
 
 module.exports = pool.promise();
->>>>>>> eb502bf00be535d061ae0d80bd7706d57f4f4bb4
