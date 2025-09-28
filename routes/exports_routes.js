61173c45f465b3d7116547bcacae2c3056ffba80
JorgeAlexandresantosfilho <jorgedbl309@gmail.com>
2025-09-28 18:54:07 -0300

    Todos os problemas com o merge foram corrigidos

--- routes/exports_routes.js
+++ routes/exports_routes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express =  require('express');
 const router = express.Router();
 const { ExportClients } = require('../controllers/exports_controller');
@@ -6,15 +6,4 @@ const { ExportClients } = require('../controllers/exports_controller');
 
 router.get("/export/clients", ExportClients);
 
-
-=======
-const express =  require('express');
-const router = express.Router();
-const { ExportClients } = require('../controllers/exports_controller');
-
-
-router.get("/export/clients", ExportClients);
-
-
->>>>>>> eb502bf00be535d061ae0d80bd7706d57f4f4bb4
 module.exports = router;
--- server.js
+++ server.js
@@ -1,34 +1,13 @@
-<<<<<<< HEAD
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
-
 
 require('dotenv').config();
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_DATABASE:', process.env.DB_DATABASE);
 
-
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Running on Port: ${PORT}`)
-=======
-const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config();
-
-
-require('dotenv').config();
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Running on Port: ${PORT}`)
->>>>>>> eb502bf00be535d061ae0d80bd7706d57f4f4bb4
 });
